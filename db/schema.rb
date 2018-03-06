a55b702496033018672836903163dee8f79f4112
ICanDoAllThingThroughChrist <ctlee9096@gmail.com>
2018-03-06 11:59:39 -0600

    completed basic User model (including secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306165047) do
+ActiveRecord::Schema.define(version: 20180306173514) do
 
   create_table "box_items", force: :cascade do |t|
     t.integer "box_id"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20180306165047) do
     t.integer "current_box_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
